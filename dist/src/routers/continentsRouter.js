56189857a2c440db9edddb38bdd215f8baa1481a
Tallispt <tallispt@gmail.com>
2022-12-13 01:42:36 -0300

    Outing dist dir

--- dist/src/routers/continentsRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var continentsRouter = (0, express_1.Router)();
-continentsRouter.get("/continents", function () { });
-continentsRouter.get("/continents/:continentId", function () { });
-continentsRouter.post("continents/", function () { });
-exports.default = continentsRouter;
